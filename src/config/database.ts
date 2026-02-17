08de04763ec85f0fc63791f411e8a9f394c11819
aamirhannan <aamirhannan08@gmail.com>
2026-02-17 19:23:39 +0530

    feat: Implement MongoDB database connection setup.

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    console.log("process.env.MONGO_URI", process.env.MONGO_URI);
+    // console.log("process.env.MONGO_URI", process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/quranlingo');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
